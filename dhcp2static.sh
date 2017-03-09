ee3739b1fc2482ed6ea76d50935d04d0d41d5008
yasinyin <976185225@qq.com>
2017-03-10 03:59:19 +0800

    fix error

--- dhcp2static.sh
+++ dhcp2static.sh
@@ -5,14 +5,17 @@ set -x
 # script for the allocation of static ip
 #
 
-#NETWORK_FILE='/etc/network/interfaces';
-#NETWORK_FILE_BAK='/etc/network/interfaces.bak';
-
-NETWORK_FILE='./interfaces';
-NETWORK_FILE_BAK='./interfaces.bak';
+NETWORK_FILE='/etc/network/interfaces';
+NETWORK_FILE_BAK='/etc/network/interfaces.bak';
+DHCP_FILE='./interfaces.bak';
 
 echo "backup old interface. . . "
-#mv $NETWORK_FILE $NETWORK_FILE_BAK
+mv $NETWORK_FILE $NETWORK_FILE_BAK
 read -p "Enter the ip you want to set for this cvm:" n
 echo "$n"
-sed '20 c inface ens224 inet static\naddress '$n'\nnetmask 255.255.255.0' $NETWORK_FILE_BAK > $NETWORK_FILE
+sed '20 c iface ens224 inet static\naddress '$n'\nnetmask 255.255.255.0' $DHCP_FILE > $NETWORK_FILE
+ifdown ens160;ifup ens160;
+ifdown ens192;ifup ens192;
+ifdown ens224;ifup ens224;
+sleep 3;
+ip a;
